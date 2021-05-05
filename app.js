01014fdfd5e2912ca83a0774605ff0cab07c90a4
Hardeep <aulakh.hardeep91@gmail.com>
2021-05-04 22:40:30 -0400

    Added questions using inquirer module

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require ('inquirer');
+inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
 
-const profileDataArgs = process.argv.slice(2);
-console.log(profileDataArgs);
+.then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
-console.log(name, github);
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
+
+//const profileDataArgs = process.argv.slice(2);
+//console.log(profileDataArgs);
+
+//const [name, github] = profileDataArgs;
+//console.log(name, github);
 
 //const generatePage = (userName, gitHubName) => {
 //return`Name: ${userName}
 //Github:${gitHubName}`;
 //};
 //console.log(generatePage('Hardeep', 'aulakhhardeep'));
-fs.writeFile('index.html', generatePage(name, github), err =>{
-    if(err) throw err;
-console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//fs.writeFile('index.html', generatePage(name, github), err =>{
+   // if(err) throw err;
+//console.log('Portfolio complete! Check out index.html to see the output!');
+//});
 
 
 
