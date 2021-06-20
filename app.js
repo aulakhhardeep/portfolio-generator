6981b08022044d9d0b8563ce7a37bf7fa66763b6
Hardeep <aulakh.hardeep91@gmail.com>
2021-06-20 17:00:33 -0400

    feature/finished-site

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require ('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 const inquirer = require ('inquirer');
 const promptUser = () => {
@@ -147,14 +147,23 @@ Add a New Project
 };
 
 promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile('index.html', pageHTML, err =>{
-        if(err) throw err;
-        console.log('Portfolio complete! Check out index.html to see the output!');
-     });
-  });
+.then(promptProject)
+.then(portfolioData => {
+  return generatePage(portfolioData);
+})
+.then(pageHTML => {
+  return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+  console.log(writeFileResponse);
+  return copyFile();
+})
+.then(copyFileResponse => {
+  console.log(copyFileResponse);
+})
+.catch(err => {
+  console.log(err);
+});
 
 
 
